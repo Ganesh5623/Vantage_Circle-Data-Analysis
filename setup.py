7d1e2cd3d99d37f49f0e4551aa0b0f3d999db952
Ganesh5623 <128254310+Ganesh5623@users.noreply.github.com>
2023-04-18 18:29:04 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="your_app_name",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Other dependencies
+        "openpyxl==3.0.9",
+    ],
+)
